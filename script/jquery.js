94ddfab4201f35910a6032860144e7e6549531fc
Aleksandra <olaszczur93@gmail.com>
2021-06-27 13:34:33 +0200

    work on menu changing color with current section

--- script/jquery.js
+++ script/jquery.js
@@ -3,7 +3,7 @@ const $navMenu = $('nav');
 const $navAsideArrow = $('div.wrapper-nav nav ul li:nth-of-type(1) a');
 const $sectionNav = $('div.wrapper-nav');
 
-//used values for functions
+//used values for function
 const scrollValue = $(window).scrollTop();
 const sectionNavDist = $sectionNav.offset().top;
 const $navArrowHeight = parseInt($navAsideArrow.css('height'));
@@ -24,32 +24,38 @@ $(document).on("scroll", scrollThroughPage);
 
 
 //menu shows currently viewed section with different colors
-// function menuChangeColor() {
+//used values for function
+    const heightNav2 = $(".skills").offset().top;
+    const heightNav3 = $(".projects").offset().top;
+    const heightNav4 = $(".hobby").offset().top;
+    const heightNav5 = $(".contact").offset().top;
+
+function menuChangeColor() {
   
-//     const heightNav2 = $(".skills").offset().top;
-//     const heightNav3 = $(".projects").offset().top;
-//     const heightNav4 = $(".hobby").offset().top;
-//     const heightNav5 = $(".contact").offset().top;
+    // const heightNav2 = $(".skills").offset().top;
+    // const heightNav3 = $(".projects").offset().top;
+    // const heightNav4 = $(".hobby").offset().top;
+    // const heightNav5 = $(".contact").offset().top;
   
-//     if (scrollValue < heightNav2) {
-//       $("li").not(".profile").removeClass("active");
-//       $(".profile").addClass("active");
-//     } else if (scrollValue < heightNav3) {
-//       $("li").not(".skills").removeClass("active");
-//       $(".skills").addClass("active");
-//     } else if (scrollValue < heightNav4) {
-//       $("li").not(".projects").removeClass("active");
-//       $(".projjects").addClass("active");
-//     } else if (scrollValue < heightNav5) {
-//       $("li").not(".hobby").removeClass("active");
-//       $(".hobby").addClass("active");
-//     } else {
-//       $("li").not(".contact").removeClass("active");
-//       $(".contact").addClass("active");
-//     }
-//   }
+    if (scrollValue < heightNav2) {
+      $("li").not(".profile").removeClass("active");
+      $(".profile").addClass("active");
+    } else if (scrollValue < heightNav3) {
+      $("li").not(".skills").removeClass("active");
+      $(".skills").addClass("active");
+    } else if (scrollValue < heightNav4) {
+      $("li").not(".projects").removeClass("active");
+      $(".projjects").addClass("active");
+    } else if (scrollValue < heightNav5) {
+      $("li").not(".hobby").removeClass("active");
+      $(".hobby").addClass("active");
+    } else {
+      $("li").not(".contact").removeClass("active");
+      $(".contact").addClass("active");
+    }
+  }
   
-// $(window).on("scroll", menuChangeColor);
+$(window).on("scroll", menuChangeColor);
 //end
 
 //links from menu point smothly to the section
